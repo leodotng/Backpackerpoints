624e9b8ef9c11b8a814912c92f503ab7c7dbfda7
leodotng <josh@leo.ng>
2018-01-23 11:15:44 -0700

    added okta

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Security, ImplicitCallback } from '@okta/okta-react';
+
+
+const config = {
+  issuer: 'https://dev-526103.oktapreview.com/oauth2/default',
+  redirect_uri: window.location.origin + '/implicit/callback',
+  client_id: '{0oadqlnatb1TIjCRw0h7}' 
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          
+          <h1 className="App-title">Welcome to Backpackerpoints</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
