690cdd9ff499a9c4cac3ccf690823065120866d6
leodotng <josh@leo.ng>
2018-01-24 16:08:43 -0700

    working social logins

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Switch, Router, Route } from 'react-router-dom'
 import { Security, ImplicitCallback } from "@okta/okta-react";
 import "reactsymbols-kit/ReactSymbolsKit.css";
 import { RSButton } from "reactsymbols-kit";
 // import mapboxgl from "mapbox-gl";
 import Home from "./Home";
 import Box from './components/Box';
+import BuyPoints from './components/BuyPoints';
+import MyBackpack from './components/MyBackpack';
 import './App.css';
 import Logo from "./images/bp.png";
 import ReactGA from "react-ga";
-import Socialbutton from './components/SocialButton';
 import SocialButton from "./components/SocialButton";
+import InstagramLogin from 'react-instagram-login';
 ReactGA.initialize("UA-112995308-1");
 ReactGA.pageview(window.location.pathname + window.location.search);
+
+
+const responseInstagram = (response) => {
+  console.log(response);
+}
 
 const handleSocialLogin = (user) => {
   console.log(user)
@@ -60,6 +68,14 @@ class App extends Component {
   render() {
     return (
       <div>
+        <main>
+{/* <Switch>
+  <Route exact path='/' component={App}/>
+  
+  <Route path='/BuyPoints' component={BuyPoints}/>
+  <Route path='/MyBackpack' component={MyBackpack}/>
+</Switch> */}
+</main>
         <div>
           <img src={require("./images/bpack.png")} style={pStyle} />
           </div>
@@ -84,12 +100,22 @@ class App extends Component {
           </SocialButton>
           <SocialButton
           provider='google'
-          appId='backpackerpoints-193120'
+          appId='440198501776-sbtfoml6df7badr0smbeq6gekknedeih.apps.googleusercontent.com'
           onLoginSuccess={handleSocialLogin}
           onLoginFailure={handleSocialLoginFailure}
           >
          <img src={require("./images/google.png")} style={socialB} />
           </SocialButton>
+          <br />
+          <InstagramLogin
+    clientId="fa4d833b7db84483a21b13423af3bac5"
+    // buttonText="Login"
+    onSuccess={responseInstagram}
+    onFailure={responseInstagram}
+    style={bPoints}
+  />
+         {/* <img src={require("./images/instagram.png")} style={socialB} />
+          </InstagramLogin> */}
         </div>
         <div>
         <Box />
@@ -104,7 +130,7 @@ We are partnering with hostels and adventure tours around the world to accept ou
               </p>
             </div>
         
-        <Router>
+        {/* <Router>
         <Security issuer={config.issuer}
                   client_id={config.client_id}
                   redirect_uri={config.redirect_uri}
@@ -112,7 +138,7 @@ We are partnering with hostels and adventure tours around the world to accept ou
         <Route path='/' exact={true} component={Home}/>
         <Route path='/implicit/callback' component={ImplicitCallback}/>
         </Security>
-      </Router>
+      </Router> */}
       </div>
     );
   }
